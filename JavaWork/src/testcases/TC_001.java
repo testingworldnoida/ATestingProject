beb3dc23cba4c7c3d473a4a48ed04e477b256f82
TestingWorld <TestingWorld@TestingWorld-PC>
2016-02-20 09:43:23 +0530

    New Test Case updated

---
+++ JavaWork/src/testcases/TC_001.java
@@ -0,0 +1,43 @@
+package testcases;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import com.thoughtworks.selenium.*;
+
+import java.util.regex.Pattern;
+
+public class TC_001 {
+	private Selenium selenium;
+
+	@BeforeMethod(alwaysRun=true)
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*googlechrome", "http://www.thetestingworld.com/");
+		selenium.start();
+	}
+
+	@Test(groups={"P2","Smoke"})
+	public void testUntitled() throws Exception {
+	    System.out.println("P2, Smoke");
+		selenium.open("/");
+		selenium.click("link=Step By Step");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=Training");
+		selenium.waitForPageToLoad("30000");
+	}
+
+	@Test(groups={"P1"})
+	public void testUntitled1() throws Exception {
+	    System.out.println("P1");
+		selenium.open("/");
+		selenium.click("link=Step By Step");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=Training");
+		selenium.waitForPageToLoad("30000");
+	}
+	
+	
+	@AfterMethod(alwaysRun=true)
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
